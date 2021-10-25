8253fef9259963c6e94104b4741bb91735c82d83
AGozukara <75181630+AGozukara@users.noreply.github.com>
2021-10-25 18:19:03 +0300

    Update a_faq-2.js

--- commands/a_faq-2.js
+++ commands/a_faq-2.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
   message.delete();
   const faq = new Discord.MessageEmbed()
     .setColor("BLUE")
-    .setTitle("How to buy $BRIGHT?")
+    .setTitle("**How to buy $BRIGHT?**")
     .setDescription("Please check the following video to learn how to buy $BRIGHT on Uniswap: \n [Youtube](https://www.youtube.com/watch?v=QsDpk4T0TII&t=9s) \n\n $BRIGHT also available on MEXC. Here is the link to buy on MEXC: \n [MEXC](https://www.mexc.com/exchange/BRIGHT_USDT)")
     .addField('CoinGecko Link', '[CoinGecko](https://www.coingecko.com/tr/coins/bright-union)', true)
     .addField('CoinMarketCap Link:', '[CoinMarketCap](https://coinmarketcap.com/currencies/bright-union/)', true)
