af036fd26fee0479b1efc4fe812abc38a8ec52b6
AGozukara <75181630+AGozukara@users.noreply.github.com>
2021-10-25 17:42:36 +0300

    Update a_faq-3.js

--- commands/a_faq-3.js
+++ commands/a_faq-3.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
   const faq = new Discord.MessageEmbed()
     .setColor("BLUE")
     .setTitle("**How to stake?**")
-    .setDescription("Here is a step by step guide: \n 1. Go to Stake BRIGHT in the Bright Union application.\n 2. Connect to your MetaMask(or Other wallets that supports ethereum chain). \n3. Press “Stake BRIGHT” and select an amount. Then, Leave a decent amount of ETH in your wallet to be able to pay the fee to stake. \n4. Approve spending of BRIGHT token in your MetaMask wallet. \n5. Approve the transaction in your MetaMask wallet. \n6. Your wallet now contains stkBRIGHT as a proof you staked BRIGHT (these tokens can now be send to another wallet if required). \n\nCongratulations 🥳 , your tokens are now earning rewards. Your rewards will be automatically accumulated.\n\nFor an even more detailed guide, follow the link below: \n\n📍 https://brightunion.medium.com/earn-for-creating-a-brighter-future-bright-staking-explained-55e0e13860ad \n\nAlso, if you're still confused we have a Youtube video showing it in action: \n\📍https://www.youtube.com/watch?v=QN-lVsi1W3E :")
+    .setDescription("Here is a step by step guide for you!  \n\n 1. Go to Stake BRIGHT in the Bright Union application.\n 2. Connect to your MetaMask(or Other wallets that supports ethereum chain). \n3. Press “Stake BRIGHT” and select an amount. Then, Leave a decent amount of ETH in your wallet to be able to pay the fee to stake. \n4. Approve spending of BRIGHT token in your MetaMask wallet. \n5. Approve the transaction in your MetaMask wallet. \n6. Your wallet now contains stkBRIGHT as a proof you staked BRIGHT (these tokens can now be send to another wallet if required). \n\nCongratulations 🥳 , your tokens are now earning rewards. Your rewards will be automatically accumulated.\n\nFor an even more detailed guide, follow the link below: \n\n📍 https://brightunion.medium.com/earn-for-creating-a-brighter-future-bright-staking-explained-55e0e13860ad \n\nAlso, if you're still confused we have a Youtube video showing it in action: \n\📍https://www.youtube.com/watch?v=QN-lVsi1W3E :")
     .addField('Bright Union', '[APP](https://app.brightunion.io/staking_bright)', true)
     message.channel.send(faq);
 };
@@ -19,7 +19,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'faq-stake',
+  name: 'stake',
   description: 'Admin command.',
   usage: ''
 };
