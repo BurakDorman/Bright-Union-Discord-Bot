84f4579a3ef41761410d44b740f85a212e21240a
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2021-10-25 16:21:09 +0300

    Update u_verify.js

--- commands/u_verify.js
+++ commands/u_verify.js
@@ -13,7 +13,7 @@ exports.run = async (client, message, args) => {
     const welcome = new Discord.MessageEmbed()
       .setColor("BLUE")
       .setAuthor("Bright Union DeFi Insurance Aggregator", "https://brightunion.io/wp-content/uploads/2021/10/05183502/bright-icon-small.svg", "https://brightunion.io")
-      .setDescription("You account is verified at Bright Union.\n\n **•** I'll drop down our socials and profiles, so you can stay tuned with us! Share our profiles with your friends and give vote 'Real' for our upcoming events on CoinMarketCal!")
+      .setDescription("You account is verified at Bright Union.\n\n **•** I'll drop down our socials and profiles, so you can stay tuned with us! Share our profiles with your friends and give vote 'Real' for our upcoming events on CoinMarketCal!\n \n")
       .addFields(
     	  { name: "Official Website", value: "[Info, app and more.](https://brightunion.io/)", inline: true },
     	  { name: "CoinMarketCal", value: "[Upcoming events.](https://coinmarketcal.com/en/coin/bright-union)", inline: true },
@@ -21,7 +21,7 @@ exports.run = async (client, message, args) => {
     	  { name: "Medium", value: "[News and blog posts.](https://brightunion.medium.com/)", inline: true },
     	  { name: "Twitter", value: "[News and tweets.](https://twitter.com/bright_union)", inline: true },
     	  { name: "Instagram", value: "[Visual posts.](https://www.instagram.com/brightunion/)", inline: true },
-		  )
+	)
     message.member.send(welcome)
     
     };
