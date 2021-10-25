88f4318b6f891a1385f2576ea434839c8ff94b8d
AGozukara <75181630+AGozukara@users.noreply.github.com>
2021-10-25 12:15:02 +0300

    Update a_faq-2.js

--- commands/a_faq-2.js
+++ commands/a_faq-2.js
@@ -6,8 +6,7 @@ exports.run = (client, message, args) => {
   const faq = new Discord.MessageEmbed()
     .setColor("BLUE")
     .setTitle("How to buy $BRIGHT?")
-    .setDescription("Please check the following video to learn how to buy $BRIGHT on Uniswap: \n [Youtube](https://www.youtube.com/watch?v=QsDpk4T0TII&t=9s) ")
-    .setDescription("$BRIGHT also available on MEXC. Here is the link to buy on MEXC: \n [MEXC](https://www.mexc.com/exchange/BRIGHT_USDT) ")
+    .setDescription("Please check the following video to learn how to buy $BRIGHT on Uniswap: \n [Youtube](https://www.youtube.com/watch?v=QsDpk4T0TII&t=9s) \n\n $BRIGHT also available on MEXC. Here is the link to buy on MEXC: \n [MEXC](https://www.mexc.com/exchange/BRIGHT_USDT)")
     
   message.channel.send(faq);
 };
