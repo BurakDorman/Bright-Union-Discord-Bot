f8febcae2044255f9d3156d7e28ffd1caf747ae0
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2021-10-25 13:20:42 +0300

    Update a_clear.js

--- commands/a_clear.js
+++ commands/a_clear.js
@@ -1,36 +1,34 @@
 const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
 
 exports.run = function(client, message, args) {
   if (message.guild !== null)
-  if (message.guild.id === "826925131493933127") {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(`Bu komutu kullanabilmek için **Mesajları Yönet** iznine sahip olmalısın!`);
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(`You should have **Manage Messages** permission!`);
     if(isNaN(args[0])) {
       var errembed = new Discord.MessageEmbed()
         .setColor("RANDOM")
-        .addField(`Hata!`, `Bir sayı yazmalısın.`)
-        .addField(`Doğru Kullanım:`, `${ayarlar.prefix}sil <temizlenecek mesaj sayısı>`)
+        .addField(`Error!`, `You must input an integer.`)
+        .addField(`Usage:`, `${process.env.prefix}clear <amount of messages to be deleted>`)
       return message.channel.send(errembed);
     }
-    if (args[0] < 1) return message.reply("**1** adetten az mesaj silemem!")
-    if (args[0] > 100) return message.reply("**100** adetten fazla mesaj silemem!")
+    if (args[0] < 1) return message.reply("Input cannot be less than **1**!")
+    if (args[0] > 100) return message.reply("Input cannot be more than **100**!")
     message.channel.bulkDelete(args[0]).then(deletedMessages => {
-      if (deletedMessages.size < 1) return message.reply("Hiç mesaj silemedim! _(**14** günden eski mesajları silemem!)_");
+      if (deletedMessages.size < 1) return message.reply("I couldn't delete any messages! _(I cannot delete messages more than **14** days old!)_");
     })
-    message.channel.send(`**${args[0]}** adet mesaj silindi!`).then(msg => msg.delete({timeout: 4000}));
+    message.channel.send(`**${args[0]}** messages have been deleted!`).then(msg => msg.delete({timeout: 4000}));
   }
 };
 
 exports.conf = {
   enabled: true, 
   guildOnly: false, 
-  aliases: ["temizle", "mesaj-sil", "mesajları-sil"],
-  permLevel: 0
+  aliases: [],
+  permLevel: 1
 };
 
 exports.help = {
-  name: 'sil',
-  category: 'moderasyon',
-  description: 'Belirtilen miktarda mesaj siler.',
-  usage: '!sil <miktar>'
+  name: 'clear',
+  category: 'moderation',
+  description: 'Deletes specified amount of messages.',
+  usage: '!clear <amount>'
 };
