0b6d6317167de40579a989e1bab63ee1e15971b9
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2021-10-25 15:53:13 +0300

    Update u_verify.js

--- commands/u_verify.js
+++ commands/u_verify.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
   if (message.guild !== null)
   var role = message.guild.roles.cache.find(role => role.id === "902146856405057576");
   if (message.channel.id === "901146961384710185") {
-    message.member.addRole(role);
+    message.member.roles.add(role);
     //message.author.id.roles.add("902146856405057576")
     //message.channel.send("${message.author} has been verified!").then(msg => msg.delete({timeout: 5000}))
     };
