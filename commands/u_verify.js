503db39fccd23ec8444324972fb77ff3a71472cf
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2021-10-25 15:38:49 +0300

    Update u_verify.js

--- commands/u_verify.js
+++ commands/u_verify.js
@@ -4,7 +4,7 @@ const db = require("quick.db");
 exports.run = async (client, message, args) => {
   if (message.guild !== null)
   if (message.channel.id === "901146961384710185") {
-    message.guild.members.cache.find.roles.add("902146856405057576").then(msg => msg.delete({timeout: 5000}));
+    message.author.roles.add("902146856405057576").then(msg => msg.delete({timeout: 5000}));
     };
     return
 }
