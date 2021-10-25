f6f06b5feca11b91aff691ec6b1da63247c88aac
AGozukara <75181630+AGozukara@users.noreply.github.com>
2021-10-25 18:18:46 +0300

    Update a_faq-4.js

--- commands/a_faq-4.js
+++ commands/a_faq-4.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
   message.delete();
   const faq = new Discord.MessageEmbed()
     .setColor("BLUE")
-    .setTitle("Unstake Procedure: ")
+    .setTitle("**Unstake Procedure: **")
 
     .setDescription("\n\n There is a 7-day cool-down period if you want to unstake your $BRIGHT tokens.\n Then, You will have 2 days to withdraw your tokens. If you do not withdraw them in a given time, $BRIGHT tokens will go to the staking pool automatically.")
  
