ca65a5616d4766935e97873bb162cbc2136d45e9
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2021-10-25 15:33:27 +0300

    Add files via upload

---
+++ commands/u_verify.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+exports.run = async (client, message, args) => {
+  if (message.guild !== null)
+  if (message.channel.id === "901146961384710185") {
+    member.roles.add("902146856405057576")
+    };
+    return
+}
+
+exports.conf = {
+  enabled: true, 
+  guildOnly: true, 
+  aliases: [],
+  permLevel: `No need for any permission.` 
+};
+
+exports.help = {
+  name: 'verify',
+  category: '',
+  description: '',
+  usage:''
+}
