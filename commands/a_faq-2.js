a67e90506fbb3e7efc82019bca39ca731ec5d8c6
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2021-10-28 17:37:49 +0300

    Update a_faq-2.js

--- commands/a_faq-2.js
+++ commands/a_faq-2.js
@@ -2,14 +2,21 @@ const Discord = require('discord.js');
 const db = require('quick.db');
 
 exports.run = (client, message, args) => {
-  message.delete();
-  const faq = new Discord.MessageEmbed()
-    .setColor("BLUE")
-    .setTitle("**How to buy $BRIGHT?**")
-    .setDescription("Please check the following video to learn how to buy $BRIGHT on Uniswap: \n [Youtube](https://www.youtube.com/watch?v=QsDpk4T0TII&t=9s) \n\n $BRIGHT also available on MEXC. Here is the link to buy on MEXC: \n [MEXC](https://www.mexc.com/exchange/BRIGHT_USDT)")
-    .addField('CoinGecko Link', '[CoinGecko](https://www.coingecko.com/tr/coins/bright-union)', true)
-    .addField('CoinMarketCap Link:', '[CoinMarketCap](https://coinmarketcap.com/currencies/bright-union/)', true)
-  message.channel.send(faq);
+  if (message.guild !== null)
+  if (message.guild.id === "890247463817072671")
+  let current = message.channel.id
+  let bot = "902144996147355688"
+  let faq = "890247978038726677"
+  if (current === bot || current === faq) {
+    message.delete();
+    const faq = new Discord.MessageEmbed()
+      .setColor("BLUE")
+      .setTitle("**How to buy <:bright:903289521381187635>$BRIGHT?**")
+      .setDescription("Please check [this video](https://www.youtube.com/watch?v=QsDpk4T0TII&t=9s) to learn how to buy <:bright:903289521381187635>$BRIGHT on Uniswap:\n\n <:bright:903289521381187635>$BRIGHT also available on [MEXC](https://www.mexc.com/exchange/BRIGHT_USDT).\n�")
+      .addField('CoinGecko Link', '[CoinGecko](https://www.coingecko.com/tr/coins/bright-union)', true)
+      .addField('CoinMarketCap Link:', '[CoinMarketCap](https://coinmarketcap.com/currencies/bright-union/)', true)
+    message.channel.send(faq);
+  }
 };
 
 exports.conf = {
