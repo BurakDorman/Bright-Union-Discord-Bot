c4a469a309a3d3e0286f2d4f1d94825309df0683
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2021-10-28 18:11:46 +0300

    Update a_faq-buy.js

--- commands/a_faq-buy.js
+++ commands/a_faq-buy.js
@@ -7,8 +7,9 @@ exports.run = (client, message, args) => {
   if (message.channel.id === "902144996147355688" || message.channel.id === "890247978038726677") {
     message.delete();
     const faq = new Discord.MessageEmbed()
-      .setColor("BLUE")
+      .setColor("#00a5ff")
       .setTitle("How to buy $BRIGHT?")
+      .setURL("https://docs.brightunion.io/how-to-guide/buy-bright")
       .setDescription("<:uniswap:903294803364687923> Please check [this video](https://www.youtube.com/watch?v=QsDpk4T0TII&t=9s) to learn how to buy [$BRIGHT](https://coinmarketcap.com/currencies/bright-union/) on Uniswap.\n\n<:mexc:903294303537881130> [$BRIGHT](https://www.coingecko.com/coins/bright-union) is also available on [MEXC](https://www.mexc.com/exchange/BRIGHT_USDT).\n \n<:bright:903289521381187635> For price tracking and more:")
       .addField('CoinGecko', '[Add to your favourites.](https://www.coingecko.com/coins/bright-union)', true)
       .addField('CoinMarketCap', '[Add to your watchlist.](https://coinmarketcap.com/currencies/bright-union/)', true)
