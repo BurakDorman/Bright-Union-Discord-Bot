1a507c4db8456d891fe49202589bb48b23113bb9
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2021-10-28 17:44:32 +0300

    Update a_faq-2.js

--- commands/a_faq-2.js
+++ commands/a_faq-2.js
@@ -4,17 +4,14 @@ const db = require('quick.db');
 exports.run = (client, message, args) => {
   if (message.guild !== null)
   if (message.guild.id === "890247463817072671")
-  let croom = message.channel.id
-  let broom = "902144996147355688"
-  let froom = "890247978038726677"
-  if (croom === broom || croom === froom) {
+  if (message.channel.id === "902144996147355688" || message.channel.id === "890247978038726677") {
     message.delete();
     const faq = new Discord.MessageEmbed()
       .setColor("BLUE")
-      .setTitle("**How to buy <:bright:903289521381187635>$BRIGHT?**")
+      .setTitle("How to buy <:bright:903289521381187635>$BRIGHT?")
       .setDescription("Please check [this video](https://www.youtube.com/watch?v=QsDpk4T0TII&t=9s) to learn how to buy <:bright:903289521381187635>$BRIGHT on Uniswap:\n\n <:bright:903289521381187635>$BRIGHT also available on [MEXC](https://www.mexc.com/exchange/BRIGHT_USDT).\n�")
-      .addField('CoinGecko Link', '[CoinGecko](https://www.coingecko.com/tr/coins/bright-union)', true)
-      .addField('CoinMarketCap Link:', '[CoinMarketCap](https://coinmarketcap.com/currencies/bright-union/)', true)
+      .addField('CoinGecko', '[Token info and price.](https://www.coingecko.com/tr/coins/bright-union)', true)
+      .addField('CoinMarketCap', '[Token info and price.](https://coinmarketcap.com/currencies/bright-union/)', true)
     message.channel.send(faq);
   }
 };
