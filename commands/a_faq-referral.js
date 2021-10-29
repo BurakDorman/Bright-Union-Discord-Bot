15361fc28f17c36535f44333112de76cddcdaacf
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2021-10-29 13:39:03 +0300

    Update a_faq-referral.js

--- commands/a_faq-referral.js
+++ commands/a_faq-referral.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
   if (message.guild.id === "890247463817072671")
   if (message.channel.id === "902144996147355688" || message.channel.id === "890247978038726677") {
     const faq = new Discord.MessageEmbed()
-      .setColor("BLUE")
+      .setColor("#00a5ff")
       .setTitle("How does the referral program work?")
       .setURL("https://brightunion.medium.com/bright-union-referral-program-bfde221c949a")
       .setDescription("We are excited to announce the referral program!\nRefer a friend with your referral link and both you and your referral receive $20 worth of [$BRIGHT](https://coinmarketcap.com/currencies/bright-union/) tokens when your friend [gets coverage](https://app.brightunion.io/coverages) for a premium of $200 or more.\nTo learn more about our referral program please visit this [Medium post](https://brightunion.medium.com/bright-union-referral-program-bfde221c949a).")
