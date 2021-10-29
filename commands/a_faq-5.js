13a4e4fab863730370ea5285d60f98bd0e654939
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2021-10-29 10:00:03 +0300

    Update a_faq-5.js

--- commands/a_faq-5.js
+++ commands/a_faq-5.js
@@ -2,23 +2,28 @@ const Discord = require('discord.js');
 const db = require('quick.db');
 
 exports.run = (client, message, args) => {
-  message.delete();
-  const faq = new Discord.MessageEmbed()
-    .setColor("BLUE")
-    .setTitle("**How to Buy Decentralized Crypto Coverage?**")
-    .setDescription("  \n Check out all the covers available at app.brightunion.io/coverages and here is a tutorial for the process: \n  https://www.youtube.com/watch?v=1D1KIvjuw-Y")
-  message.channel.send(faq);
+  if (message.guild !== null)
+  if (message.guild.id === "890247463817072671")
+  if (message.channel.id === "902144996147355688" || message.channel.id === "890247978038726677") {
+    //message.delete();
+    const cover = new Discord.MessageEmbed()
+      .setColor("#00a5ff")
+      .setTitle("How to Buy Decentralized Crypto Coverage?")
+      .setURL("https://docs.brightunion.io/faq/faq-buy-cover")
+      .setDescription("You can check out all the covers available at [app.brightunion.io/coverages](https://app.brightunion.io/coverages) and [here is a tutorial video](https://www.youtube.com/watch?v=1D1KIvjuw-Y) for the process.")
+    message.channel.send(cover);
+  }
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: [],
-  permLevel: 3
+  permLevel: 0
 };
 
 exports.help = {
-  name: 'buy_coverage',
-  description: 'Admin command.',
+  name: 'buy coverage',
+  description: 'Useful command.',
   usage: ''
 };
